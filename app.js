c234521314b73deb027cfa1047bd7f6977460002
Kristof Kruller <kristof.kruller@gmail.com>
2021-02-20 09:11:41 +0100

    anim settings, page split

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ observer = new IntersectionObserver((entries) => {
 
     entries.forEach(entry => {
         if(entry.intersectionRatio > 0) {
-            entry.target.style.animation = `anim1 1.5s ${entry.target.dataset.delay} forwards ease-out`;
+            entry.target.style.animation = `anim1 1s ${entry.target.dataset.delay} forwards ease-out`;
         }
         else {
             entry.target.style.animation = 'none';
